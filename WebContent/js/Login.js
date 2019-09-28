61bbdde82ce531db6ea219a391c379acc2bc3cd7
IFallowed <kc@LAPTOP-OMT2TLCN>
2019-09-29 00:47:02 +0800

    2019-09-28 second commit

--- WebContent/js/Login.js
+++ WebContent/js/Login.js
@@ -1,4 +1,12 @@
-function barter_btn(bb){
-	$(bb).parent().hide();
-	$(bb).parent().siblings().show();
-}
+//function barter_btn(bb){
+//	$(bb).parent().hide();
+//	$(bb).parent().siblings().show();
+//}
+$(function () {
+    $('#form-tab a:first').tab('show')
+});
+$('#login_mail').blur(function() {
+	if($(this).val().trim().length < 1){
+		$(this).find('.form_info').html("请填写邮箱！");
+	}
+});
